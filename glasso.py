140f21efb85544515ba34556c800f6ff309604eb
Luke Price <39465652+LucasNPrice@users.noreply.github.com>
2019-06-07 13:44:44 -0400

    Add files via upload

--- glasso.py
+++ glasso.py
@@ -35,6 +35,7 @@ class FGLasso():
         print("function j =", j)
         self.__updateTheta_Inv(j = j) # UpdateTheta in R
         self.__updateTheta(j = j, gamma = gamma, epsilon = epsilon) # Algorithm_3 in R
+        self.__updateSigma(j = j)
         # sys.exit()
 
       print('Iteration {} Time: {}'.format(iteration, time.time() - iter_start_time))
@@ -62,16 +63,16 @@ class FGLasso():
 
     while error[-1] >= epsilon:
       last_w = np.delete(self.Theta, obj = j, axis = 0)[:,j]
-      theta_nj = np.delete(self.Theta_inv, obj = j, axis = 0) # remove row j
-      theta_nj = np.delete(theta_nj, obj = j, axis = 1) # remove column j
-      theta_nj_transpose = np.transpose(theta_nj, (1, 0, 3, 2)) # transpose theta_nj
+      theta_inv_nj = np.delete(self.Theta_inv, obj = j, axis = 0) # remove row j
+      theta_inv_nj = np.delete(theta_inv_nj, obj = j, axis = 1) # remove column j
+      theta_inv_nj_transpose = np.transpose(theta_inv_nj, (1, 0, 3, 2)) # transpose theta_inv_nj
 
       for k in range(0,self.p-1):
         block_residual = np.zeros((self.M,self.M))
         for l in range(0,self.p-1):
           if k != l:
             w_block = np.delete(self.Theta, obj = j, axis = 0)[l,j]
-            inner_mult = np.matmul(theta_nj_transpose[l,k], w_block)
+            inner_mult = np.matmul(theta_inv_nj_transpose[l,k], w_block)
             block_residual += np.matmul(inner_mult, self.CorMat[j,j])
 
         fNorm = np.linalg.norm(block_residual, ord = 'fro')
@@ -83,20 +84,42 @@ class FGLasso():
         else:
           # solve system of equations for theta block
           wj[k] = fsolve(self.__wjk, x0 = np.identity(self.M), 
-            args = (theta_nj, block_residual, gamma, j, k))
+            args = (theta_inv_nj, block_residual, gamma, j, k))
 
       iter_j = np.delete(np.arange(0,self.p,1), obj = j)
       self.Theta[iter_j,j] = wj
+      # also update transpose here!
       update_diff = np.reshape(self.Theta[iter_j,j],(49*5,5)) - np.reshape(last_w,(49*5,5))
       error.append(np.linalg.norm(update_diff, ord = 'fro'))
       iteration += 1
 
-  def __wjk(self, x, theta_nj, block_residual, gamma, j, k):
+  def __wjk(self, x, theta_inv_nj, block_residual, gamma, j, k):
     w = np.reshape(x, (self.M, self.M))
-    kron_prod = np.kron(theta_nj[k,k], self.CorMat[j,j])
+    kron_prod = np.kron(theta_inv_nj[k,k], self.CorMat[j,j])
     wj = np.matmul(kron_prod, w.flatten()) + block_residual.flatten() + (
       gamma * (w.flatten() / np.linalg.norm(w, ord = 'fro')))
     return(wj)
+
+  def __updateSigma(self, j):
+    innerblock = np.zeros((self.M,self.M))
+    Uj = np.reshape(np.repeat(innerblock, self.p-1), (49,5,5))
+    theta_inv_nj = np.delete(self.Theta_inv, obj = j, axis = 0) # remove row j
+    theta_inv_nj = np.delete(theta_inv_nj, obj = j, axis = 1) # remove col j
+    theta_nj = np.delete(self.Theta_inv, obj = j, axis = 0) # remove row j
+
+    for rowblock in range(0,len(Uj)):
+      for colblock in range(0,len(Uj)):
+        Uj[colblock] += np.matmul(theta_inv_nj[rowblock,colblock], theta_nj[colblock,j])
+
+    Uj_transpose = np.transpose(Uj, (0,2,1))
+    
+    self.Sigma[j,j] = self.CorMat[j,j]
+
+    continue 
+
+
+
+
 
 
 
